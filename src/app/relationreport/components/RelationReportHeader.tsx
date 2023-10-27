8c3db8872f225b91eabef0b62c97bd5f143f60eb
eunbin.kim <116791055+kim-eunbin@users.noreply.github.com>
2023-10-27 10:25:18 +0900

    feature/hellobot-2418 feature: 관계도 페이지 제목 UI (#8)
    * fix: tailwind css 설정 안되는 이슈 수정
    
    * feature: 관계도 페이지 제목 UI
    
    ---------
    
    Co-authored-by: eunbin20 <eunbines2@gmail.com>

--- src/app/relationreport/components/RelationReportHeader.tsx
+++ src/app/relationreport/components/RelationReportHeader.tsx
@@ -1,9 +1,25 @@
+import Image from 'next/image';
 import * as React from 'react';
 
-interface IRelationReportHeaderProps {}
-
-const RelationReportHeader = (props: IRelationReportHeaderProps) => {
-  return <header>헤더</header>;
+const RelationReportHeader = () => {
+  return (
+    <header className="w-full flex justify-between px-4 pt-4">
+      <Image
+        src="/images/buttons-btn-modal-close.svg"
+        alt="Close Icon"
+        width={32}
+        height={32}
+        priority
+      />
+      <Image
+        src="/images/buttons-btn-modal-share.svg"
+        alt="Share Icon"
+        width={32}
+        height={32}
+        priority
+      />
+    </header>
+  );
 };
 
 export default RelationReportHeader;
--- src/app/relationreport/components/RelationReportTitle.tsx
+++ src/app/relationreport/components/RelationReportTitle.tsx
@@ -1,7 +1,24 @@
+import Image from 'next/image';
 import * as React from 'react';
 
-const RelationReportTitle = () => {
-  return <div>타이틀</div>;
+interface IRelationReportTitle {
+  title: string;
+}
+
+const RelationReportTitle = ({ title }: IRelationReportTitle) => {
+  return (
+    <div className="px-5 pt-6">
+      <div className="flex">
+        <h1 className="text-[22px] font-bold mr-1">{title}</h1>
+        <Image
+          src="/images/icon-modify-small.svg"
+          alt="Modify Icon"
+          width={20}
+          height={20}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default RelationReportTitle;
