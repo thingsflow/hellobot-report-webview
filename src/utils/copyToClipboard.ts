97ffb6e9f144ab8de6a018322f6acd0274c002e1
kom-eunbin <eunbin.kim@thingsflow.com>
2023-12-05 10:54:15 +0900

    fix: copyToClipboard

--- src/utils/copyToClipboard.ts
+++ src/utils/copyToClipboard.ts
@@ -1,11 +1,44 @@
-export const isClipboardSupported = () => navigator?.clipboard != null;
+const getDummyTextarea = () => {
+  const textarea = document.createElement('textarea') as HTMLTextAreaElement;
+  textarea.style.top = '0';
+  textarea.style.left = '0';
+  textarea.style.display = 'fixed';
 
-export const copyToClipboard = async (text: string) => {
-  if (isClipboardSupported()) {
-    try {
-      await navigator.clipboard.writeText(text);
-    } catch (e) {}
-  }
+  return textarea;
+};
+
+export const isClipboardSupported = () => navigator?.clipboard != null;
+export const isClipboardCommandSupported = () =>
+  document.queryCommandSupported?.('copy') ?? false;
+
+export const copyToClipboard = (text: string) => {
+  return new Promise<boolean>(async (resolve) => {
+    const rootElement = document.body;
+
+    // if (isClipboardSupported()) {
+    //   await navigator.clipboard.writeText(text);
+    //   resolve(true);
+    //   return;
+    // }
+
+    if (isClipboardCommandSupported()) {
+      const textarea = getDummyTextarea();
+      textarea.value = text;
+
+      rootElement.appendChild(textarea);
+
+      textarea.focus();
+      textarea.select();
+
+      document.execCommand('copy');
+      rootElement.removeChild(textarea);
+      resolve(true);
+      return;
+    }
+
+    resolve(false);
+    return;
+  });
 };
 
 export default copyToClipboard;
