f3b5335557e6c082843f204d56f07fb1a9037380
kom-eunbin <eunbin.kim@thingsflow.com>
2023-12-11 17:29:00 +0900

    chore: lokalise

--- src/app/relationreport/[reportSeq]/components/graph/DefaultNode.tsx
+++ src/app/relationreport/[reportSeq]/components/graph/DefaultNode.tsx
@@ -57,12 +57,7 @@ const DefaultNode: FC<NodeProps> = () => {
       isKeepAnonymous !== 'true' &&
       share === 'true'
     ) {
-      // TODO: lokalise
-      if (
-        confirm(
-          '로그인 하시겠습니까? 로그인을 하지 않고 진행하게 될 시, 관계도 및 결과가 저장되지 않습니다.',
-        )
-      ) {
+      if (confirm(t('relationshipmap_popup_login_prompt'))) {
         webview.doLoginWithRedirectUrl({
           redireactUrl:
             process.env.NEXT_PUBLIC_SKILLSTORE_URL +
